218114f6edc9f8d874696650e9909e1a6dee2ec1
Christian Helgeson <cmhhelgeson@gmail.com>
2020-12-08 22:23:43 -0500

    Tried to implement some changes that I thought would be effective but idk

--- calcServer.c
+++ calcServer.c
@@ -5,10 +5,55 @@
 #include <pthread.h>
 /* buffer size for reading lines of input from user */
 #define LINEBUF_SIZE 1024
+
+int outer_exit = 1;
+
 struct CalcInfo {
 	int clientfd;
     struct Calc *c;
 };
+
+void chat_with_client(struct Calc *calc, int infd, int outfd);
+void *worker(void *arg);
+
+void chat_with_client(struct Calc *calc, int infd, int outfd) {
+	rio_t in;
+	char linebuf[LINEBUF_SIZE];
+
+	/* wrap standard input (which is file descriptor 0) */
+	rio_readinitb(&in, infd);
+	/*
+	 * Read lines of input, evaluate them as calculator expressions,
+	 * and (if evaluation was successful) print the result of each
+	 * expression.  Quit when "quit" command is received.
+	 */
+    int inner_exit = 1;
+    while (inner_exit) {
+		ssize_t n = rio_readlineb(&in, linebuf, LINEBUF_SIZE);
+		if (n <= 0) {
+			inner_exit = 0;
+		} else if (strcmp(linebuf, "quit\n") == 0 || strcmp(linebuf, "quit\r\n") == 0) {
+			/* quit command */
+			inner_exit = 0;
+		} else if (strcmp(linebuf, "shutdown\n") == 0 || strcmp(linebuf, "shutdown\r\n") == 0){
+            inner_exit = 0;
+            outer_exit = 0;
+        } else {
+			/* process input line */
+			int result;
+			if (calc_eval(calc, linebuf, &result) == 0) {
+				/* expression couldn't be evaluated */
+				rio_writen(outfd, "Error\n", 6);
+			} else {
+				/* output result */
+				int len = snprintf(linebuf, LINEBUF_SIZE, "%d\n", result);
+				if (len < LINEBUF_SIZE) {
+					rio_writen(outfd, linebuf, len);
+				}
+			}
+		}
+	}
+}
 
 void *worker(void *arg) {
 	struct CalcInfo *info = arg;
@@ -26,43 +71,6 @@ void *worker(void *arg) {
 	return NULL;
 }
 
-int chat_with_client(struct Calc *calc, int infd, int outfd) {
-	rio_t in;
-	char linebuf[LINEBUF_SIZE];
-
-	/* wrap standard input (which is file descriptor 0) */
-	rio_readinitb(&in, infd);
-	/*
-	 * Read lines of input, evaluate them as calculator expressions,
-	 * and (if evaluation was successful) print the result of each
-	 * expression.  Quit when "quit" command is received.
-	 */
-    while (1) {
-		ssize_t n = rio_readlineb(&in, linebuf, LINEBUF_SIZE);
-		if (n <= 0) {
-			/* error or end of input */
-		} else if (strcmp(linebuf, "quit\n") == 0 || strcmp(linebuf, "quit\r\n") == 0) {
-			/* quit command */
-			return 1;
-		} else if (strcmp(linebuf, "shutdown\n") == 0 || strcmp(linebuf, "shutdown\r\n") == 0){
-            return 0;
-        } else {
-			/* process input line */
-			int result;
-			if (calc_eval(calc, linebuf, &result) == 0) {
-				/* expression couldn't be evaluated */
-				rio_writen(outfd, "Error\n", 6);
-			} else {
-				/* output result */
-				int len = snprintf(linebuf, LINEBUF_SIZE, "%d\n", result);
-				if (len < LINEBUF_SIZE) {
-					rio_writen(outfd, linebuf, len);
-				}
-			}
-		}
-	}
-    return 1;
-}
 int main(int argc, char **argv) {
     if (argc != 2) {
 		printf("Incorrect number of arguments\n");
@@ -82,8 +90,7 @@ int main(int argc, char **argv) {
         calc_destroy(calc);
         return 1;
 	}
-    int exit = 1;
-	while (exit) {
+	while (outer_exit) {
 		int clientfd = Accept(serverfd, NULL, NULL);
 		if (clientfd < 0) {
 			printf("Invalid clientfd\n");
